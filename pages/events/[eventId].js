68927a1a4fa6e66f767413d4c6d7c4a6b7a0a062
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-05 12:23:48 -0400

    Update All events page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,19 +1,18 @@
 import { Fragment } from "react";
 
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
-import ErrorAlert from "../../components/ui/error-alert";
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -40,17 +39,18 @@ export async function getStaticProps(context) {
 
   return {
     props: { selectedEvent: event },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
 
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: "blocking",
   };
 }
 
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ function HomePage(props) {
 export async function getStaticProps() {
   const featuredEvents = await getFeaturedEvents();
 
-  return { props: { events: featuredEvents } };
+  return { props: { events: featuredEvents }, revalidate: 1800 };
 }
 
 export default HomePage;
