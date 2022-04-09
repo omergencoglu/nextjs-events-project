6908dec96b06fc336f8e07f5ce51c4e970fd2fe5
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-09 12:47:24 -0400

    Newsletter error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(process.env.EVENTS_DB);
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,13 +21,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(process.env.EVENTS_DB);
+    let client;
 
-    const db = client.db();
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    await db.collection("newsletter").insertOne({ email: userEmail });
-
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
 
     res.status(201).json({ message: "Signed up!" });
   }
