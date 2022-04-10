6625236d0b142e5a716dce94eb31dbad362567a1
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-10 12:52:46 -0400

    Add notification context

---
+++ store/notification-context.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+const NotificationContext = createContext({
+  notification: null,
+  showNotification: function () {},
+  hideNotification: function () {},
+});
+
+export function NotificationContextProvider(props) {
+  return (
+    <NotificationContextProvider>{props.children}</NotificationContextProvider>
+  );
+}
+
+export default NotificationContext;
