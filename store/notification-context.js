b11ea08aefe4355470bfe2a3ffe8b20e66f52a2f
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-10 14:20:28 -0400

    Implement notification

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,14 +1,48 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NotificationContext = createContext({
-  notification: null,
-  showNotification: function () {},
+  notification: null, // { title, message, status }
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContextProvider>{props.children}</NotificationContextProvider>
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
   );
 }
 
